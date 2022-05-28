fcda7295cda3f8381a27720b891db094cea70bbd
TarrImre <tarr.imre2630@gmail.com>
2022-05-28 19:43:09 +0200

    Asszisztens kliens
    Asszisztens kliens

---
+++ WebApi_Assist/DataProviders/PersonDataProvider.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using WebApi_Common.Models;
+
+namespace WebApi_Client.DataProviders
+{
+    class PersonDataProvider
+    {
+        private const string _url = "http://localhost:5000/api/person";
+
+        public static IEnumerable<Person> GetPeople()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(_url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var rawData = response.Content.ReadAsStringAsync().Result;
+                    var people = JsonConvert.DeserializeObject<IEnumerable<Person>>(rawData);
+                    return people;
+                }
+
+                throw new InvalidOperationException(response.StatusCode.ToString());
+            }
+        }
+
+        public static void CreatePerson(Person person)
+        {
+            using (var client = new HttpClient())
+            {
+                var rawData = JsonConvert.SerializeObject(person);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PostAsync(_url, content).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+
+        public static void UpdatePerson(Person person)
+        {
+            using (var client = new HttpClient())
+            {
+                var rawData = JsonConvert.SerializeObject(person);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PostAsync(_url, content).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+
+        public static void DeletePerson(long id)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.DeleteAsync($"{_url}/{id}").Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ WebApi_Assist/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using WebApi_Client.DataProviders;
+using WebApi_Common.Models;
+
+
+namespace WebApi_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void AddPerson_Click(object sender, RoutedEventArgs args)
+        {
+            var window = new PersonWindow(null);
+            if (window.ShowDialog() ?? false)
+            {
+                UpdatePeopleListBox();
+            }
+        }
+
+        private void UpdatePeopleListBox()
+        {
+            var people = PersonDataProvider.GetPeople().ToList();
+            DataGrid.ItemsSource = people;
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdatePeopleListBox();
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            UpdatePeopleListBox();
+        }
+    }
+}
